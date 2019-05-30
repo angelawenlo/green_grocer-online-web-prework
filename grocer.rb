85947b6e2218633347ca7afd6ab5b97c2e78a2e5
Angela Lo <angelawenlo@gmail.com>
2019-05-30 19:19:39 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,40 @@
+require 'pry'
+
 def consolidate_cart(cart)
   # code here
+  hash = {}
+  cart.each do |produce_list|
+    produce_list.each do |produce, price_info|
+      if hash[produce].nil?
+        hash[produce] = price_info.merge({:count => 1})
+      else
+        hash[produce][:count] += 1
+      end
+    end
+  end
+  hash
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  hash = cart
+  cart.each do |produce, info|
+  coupons.each do |coupon_info|
+   if coupon_info[:item] == produce && info[:count] >= coupon_info[:num]
+     hash["#{produce} W/COUPON"]
+  #   info[:count] = info[:count] - coupon_info[:num]
+   #if hash[produce + "W/COUPON"]
+    # hash[produce + "W/COUPON"][:count] += 1
+   #else
+    # hash[produce + " W/COUPON"] = {
+    #   :price => coupon_info[:cost],
+    #   :clearance => produce[:clearance],
+    #   :count => 1}
+    # end
+end
+   end
+  end
+end
 end
 
 def apply_clearance(cart)
