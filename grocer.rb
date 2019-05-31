c77fda1224b208e8296efb138ac7b1fdf979e64c
Angela Lo <angelawenlo@gmail.com>
2019-05-31 14:23:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -39,7 +39,7 @@ def apply_clearance(cart)
   # code here
   cart.each do |item, item_info|
     if item_info[:clearance] == true
-      discount_price = cart[item][:price] * 20%
+      discount_price = item_info[:price] * 0.80
 
   binding.pry
     end
